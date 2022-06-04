8f0c745607b4b7f2f972f1b91da14fe603ba3469
Alan Steiman <alan.steiman@talabat.com>
2022-06-04 13:03:48 +0400

    Add init

--- Sources/GemModels/GemModels.swift
+++ Sources/GemModels/GemModels.swift
@@ -1,4 +1,9 @@
 public struct GemRestaurant: Codable {
     public let restaurantId: Int
     public let restaurantName: String
+    
+    public init(restaurantId: Int, restaurantName: String) {
+        self.restaurantId = restaurantId
+        self.restaurantName = restaurantName
+    }
 }
